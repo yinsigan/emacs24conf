2a6fa3fd729e0a25a8c5ddb57b93d4a7a79f35e4
ghosTM55 <ghostm55@shlug.org>
2012-07-22 13:39:18 +0800

    change the default theme

--- init.el
+++ init.el
@@ -35,4 +35,16 @@
 
 (set-frame-font "Menlo-12")
 
-;(load-theme 'solarized-dark t)
+(load-theme' adwaita)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
