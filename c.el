1975f35b78284a0b933f065e8e29e8d1b834a31a
ghosTM55 <ghosthomas@gmail.com>
2012-09-11 14:56:30 +0800

    update the c configuration

--- c.el
+++ c.el
@@ -2,7 +2,7 @@
 (provide 'c)
 
 (defun ghost-c-mode-common-hook()
-  (c-set-style "stroustrup")
+  (c-set-style "linux")
   (c-toggle-auto-hungry-state 1)
 )
 (add-hook 'c-mode-common-hook 'ghost-c-mode-common-hook)
